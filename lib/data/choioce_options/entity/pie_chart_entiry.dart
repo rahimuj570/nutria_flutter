52ff3b68779151fe0dc4063ee4269e24ed0b3ad1
rahimuj570 <rahimuj570@gmail.com>
2026-04-22 10:54:03 +0600

    pie_chart addedin result screen

---
+++ lib/data/choioce_options/entity/pie_chart_entiry.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class PieChartEntiry {
+  final String title;
+  double? value;
+  Color color;
+
+  PieChartEntiry({required this.title, this.value, required this.color});
+}
--- lib/presentations/chooce_options/views/screens/choosen_final_result_screen.dart
+++ lib/presentations/chooce_options/views/screens/choosen_final_result_screen.dart
@@ -1,10 +1,52 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:neutria/app/app_assets_path.dart';
+import 'package:neutria/app/const_colors.dart';
+import 'package:neutria/data/choioce_options/entity/pie_chart_entiry.dart';
 import 'package:neutria/presentations/chooce_options/viewmodels/choose_option_provider.dart';
 import 'package:provider/provider.dart';
 
-class ChoosenFinalResultScreen extends StatelessWidget {
+class ChoosenFinalResultScreen extends StatefulWidget {
   const ChoosenFinalResultScreen({super.key});
+
+  @override
+  State<ChoosenFinalResultScreen> createState() =>
+      _ChoosenFinalResultScreenState();
+}
+
+class _ChoosenFinalResultScreenState extends State<ChoosenFinalResultScreen> {
+  double _healthScore = 0;
+
+  late final List<PieChartEntiry> _pieData = [
+    PieChartEntiry(title: 'Carbs', color: Colors.grey, value: 1),
+    PieChartEntiry(title: 'Protien', color: Color(0xffFFAE4C), value: 0),
+    PieChartEntiry(title: 'Fats', color: Color(0xff6ADEF5), value: 0),
+  ];
+
+  void updateHEaltScore(double healthScore) async {
+    await Future.delayed(Duration(milliseconds: 200));
+    setState(() {
+      _healthScore = healthScore;
+    });
+  }
+
+  void updatePieScore() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    setState(() {
+      _pieData[0].color = Color(0xffFF938B);
+      _pieData[0].value = 50;
+      _pieData[1].value = 30;
+      _pieData[2].value = 20;
+    });
+  }
+
+  @override
+  void initState() {
+    updateHEaltScore(70);
+    updatePieScore();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +88,7 @@ class ChoosenFinalResultScreen extends StatelessWidget {
           ),
           SizedBox(height: 20),
           AspectRatio(
-            aspectRatio: 2 / 1.6,
+            aspectRatio: 2 / 1.4,
             child: Container(
               padding: .all(5),
               decoration: BoxDecoration(
@@ -68,11 +110,63 @@ class ChoosenFinalResultScreen extends StatelessWidget {
                     mainAxisAlignment: .spaceBetween,
                     crossAxisAlignment: .center,
                     children: [
-                      Image.asset(AppAssetsPath.pieChartResult),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 18.0),
+                        child: SizedBox(
+                          width: 150,
+                          height: 150,
+                          child: PieChart(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                            PieChartData(
+                              sectionsSpace: 0,
+                              sections: _pieData
+                                  .map(
+                                    (e) => PieChartSectionData(
+                                      radius: 28,
+                                      value: e.value,
+                                      color: e.color,
+                                      showTitle: false,
+                                      badgePositionPercentageOffset: .9,
+                                      badgeWidget: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: .circular(8),
+                                          color: Colors.grey.shade200,
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(5.0),
+                                          child: Column(
+                                            mainAxisSize: .min,
+                                            children: [
+                                              Text(
+                                                e.title,
+                                                style: TextStyle(
+                                                  fontSize: 10,
+                                                  fontWeight: .w500,
+                                                ),
+                                              ),
+                                              Text(
+                                                '${e.value}%',
+                                                style: TextStyle(
+                                                  fontSize: 12,
+                                                  fontWeight: .w600,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                        ),
+                      ),
                       SizedBox(
-                        height: 200,
                         width: 120,
                         child: Column(
+                          mainAxisSize: .min,
                           children: [
                             ListTile(
                               dense: true,
@@ -158,11 +252,15 @@ class ChoosenFinalResultScreen extends StatelessWidget {
                   ],
                 ),
                 Spacer(),
-                CircularProgressIndicator(
-                  value: .7,
-                  backgroundColor: Colors.grey.shade300,
-                  color: Colors.black,
-                  strokeWidth: 10,
+                TweenAnimationBuilder(
+                  duration: Duration(milliseconds: 300),
+                  tween: Tween(begin: 0, end: _healthScore),
+                  builder: (context, value, child) => CircularProgressIndicator(
+                    value: value / 100,
+                    backgroundColor: Colors.grey.shade300,
+                    color: Colors.black,
+                    strokeWidth: 10,
+                  ),
                 ),
               ],
             ),
@@ -182,7 +280,7 @@ class ChoosenFinalResultScreen extends StatelessWidget {
             shrinkWrap: true, // only take needed space
             physics: NeverScrollableScrollPhysics(), // no scrolling inside
             itemCount: strategies.length,
-            separatorBuilder: (_, __) => const SizedBox(height: 8),
+            separatorBuilder: (_, _) => const SizedBox(height: 8),
             itemBuilder: (context, index) => Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
